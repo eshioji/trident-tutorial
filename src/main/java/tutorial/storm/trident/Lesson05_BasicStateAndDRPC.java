125cf3877440be247e9ac1b11d26c54be5aed4c6
Enno Shioji <eshioji@gmail.com>
2013-10-31 11:09:38 +0000

    Add "join and merges"

--- src/main/java/tutorial/storm/trident/Lesson05_BasicStateAndDRPC.java
+++ src/main/java/tutorial/storm/trident/Lesson05_BasicStateAndDRPC.java
@@ -23,8 +23,8 @@ import java.io.IOException;
 /**
  * @author Enno Shioji (enno.shioji@peerindex.com)
  */
-public class Lesson04_BasicStateAndDRPC {
-    private static final Logger log = LoggerFactory.getLogger(Lesson04_BasicStateAndDRPC.class);
+public class Lesson05_BasicStateAndDRPC {
+    private static final Logger log = LoggerFactory.getLogger(Lesson05_BasicStateAndDRPC.class);
 
     public static void main(String[] args) throws Exception{
         FakeTweetGenerator fakeTweets = new FakeTweetGenerator();
