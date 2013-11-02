7e072d583fc1825785fcc4f18b37b850c271a03c
Enno Shioji <eshioji@gmail.com>
2013-11-02 15:36:38 +0000

    Example content extraction

---
+++ src/main/java/tutorial/storm/trident/testutil/Content.java
@@ -0,0 +1,45 @@
+package tutorial.storm.trident.testutil;
+
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.jvm.hotspot.jdi.ShortValueImpl;
+
+
+/**
+ * @author Enno Shioji (enno.shioji@peerindex.com)
+ */
+public class Content {
+    private static final Logger log = LoggerFactory.getLogger(Content.class);
+
+    private long tweetId;
+    private long userId;
+    private long createdAtMs;
+
+    private String contentName;
+    private String contentType;
+
+    public Content(long tweetId, long userId, long createdAtMs) {
+        this.tweetId = tweetId;
+        this.userId = userId;
+        this.createdAtMs = createdAtMs;
+    }
+
+    public String getContentId(){
+        HashFunction md5 = Hashing.md5();
+        return md5.hashString(contentType + contentName).toString();
+    }
+
+    public String getContentName() {
+        return contentName;
+    }
+
+    public void setContentName(String contentName) {
+        this.contentName = contentName;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+}
