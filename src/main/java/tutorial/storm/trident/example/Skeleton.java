aa45f10f8de951f4f0d4325f9b4b88d88fd01596
Enno Shioji <eshioji@gmail.com>
2013-11-02 15:26:23 +0000

    Make kafka broker host configurable

--- src/main/java/tutorial/storm/trident/example/Skeleton.java
+++ src/main/java/tutorial/storm/trident/example/Skeleton.java
@@ -4,16 +4,17 @@ import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.LocalDRPC;
 import backtype.storm.generated.StormTopology;
+import backtype.storm.spout.SchemeAsMultiScheme;
 import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Values;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import storm.trident.TridentState;
+import storm.kafka.KafkaConfig;
+import storm.kafka.KafkaSpout;
+import storm.kafka.StringScheme;
+import storm.kafka.trident.TransactionalTridentKafkaSpout;
+import storm.kafka.trident.TridentKafkaConfig;
 import storm.trident.TridentTopology;
-import storm.trident.operation.builtin.Count;
-import storm.trident.operation.builtin.Debug;
-import storm.trident.operation.builtin.MapGet;
 import storm.trident.testing.FeederBatchSpout;
-import storm.trident.testing.MemoryMapState;
 import tutorial.storm.trident.operations.DebugFilter;
 
 import java.io.IOException;
@@ -25,48 +26,65 @@ import java.io.IOException;
  */
 public class Skeleton {
 
-    public static StormTopology buildTopology(LocalDRPC drpc, FeederBatchSpout spout) throws IOException {
+    public static StormTopology buildTopology(LocalDRPC drpc, TransactionalTridentKafkaSpout spout) throws IOException {
 
         TridentTopology topology = new TridentTopology();
+        topology.newStream("tweets", spout)
+                .each(new Fields("str"), new DebugFilter());
 
-        TridentState countState =
-        topology
-                .newStream("spout", spout)
-                .each(new Fields("actor"), new DebugFilter())
-                .groupBy(new Fields("actor"))
-                .persistentAggregate(new MemoryMapState.Factory(), new Count(), new Fields("count"))
-        ;
-
-        topology
-                .newDRPCStream("actor_count", drpc)
-                .stateQuery(countState, new Fields("args"), new MapGet(), new Fields("count"));
+//        TridentState countState =
+//        topology
+//                .newStream("spout", spout)
+//                .each(new Fields("actor"), new DebugFilter())
+//                .groupBy(new Fields("actor"))
+//                .persistentAggregate(new MemoryMapState.Factory(), new Count(), new Fields("count"))
+//        ;
+//
+//        topology
+//                .newDRPCStream("actor_count", drpc)
+//                .stateQuery(countState, new Fields("args"), new MapGet(), new Fields("count"));
 
         return topology.build();
     }
 
     public static void main(String[] args) throws Exception {
+        Preconditions.checkArgument(args.length == 1, "Please specify the test kafka broker host:port");
+        String testKafkaBrokerHost = args[0];
+
+        TransactionalTridentKafkaSpout tweetSpout = tweetSpout(testKafkaBrokerHost);
+
         Config conf = new Config();
 
         LocalDRPC drpc = new LocalDRPC();
         LocalCluster cluster = new LocalCluster();
-        FeederBatchSpout spout = new FeederBatchSpout(ImmutableList.of("actor"));
+//        FeederBatchSpout spout = new FeederBatchSpout(ImmutableList.of("actor"));
 
-        cluster.submitTopology("hackaton", conf, buildTopology(drpc,spout));
+        cluster.submitTopology("hackaton", conf, buildTopology(drpc, tweetSpout));
 
-        spout.feed(new Values(ImmutableList.of("rose")));
-        spout.feed(new Values(ImmutableList.of("rose")));
-        spout.feed(new Values(ImmutableList.of("rose")));
-
-        spout.feed(new Values(ImmutableList.of("fred")));
-        spout.feed(new Values(ImmutableList.of("fred")));
-        spout.feed(new Values(ImmutableList.of("fred")));
-        spout.feed(new Values(ImmutableList.of("fred")));
-
-        spout.feed(new Values(ImmutableList.of("steve")));
-        spout.feed(new Values(ImmutableList.of("steve")));
-
-
-        System.out.println(drpc.execute("actor_count","rose"));
+//        spout.feed(new Values(ImmutableList.of("rose")));
+//        spout.feed(new Values(ImmutableList.of("rose")));
+//        spout.feed(new Values(ImmutableList.of("rose")));
+//
+//        spout.feed(new Values(ImmutableList.of("fred")));
+//        spout.feed(new Values(ImmutableList.of("fred")));
+//        spout.feed(new Values(ImmutableList.of("fred")));
+//        spout.feed(new Values(ImmutableList.of("fred")));
+//
+//        spout.feed(new Values(ImmutableList.of("steve")));
+//        spout.feed(new Values(ImmutableList.of("steve")));
+//
+//
+//        System.out.println(drpc.execute("actor_count","rose"));
 
     }
+
+    private static TransactionalTridentKafkaSpout tweetSpout(String testKafkaBrokerHost) {
+//        TweetIngestor ingestor = new TweetIngestor("/tmp/kafka", "test", 12000);
+//        ingestor.startAndWait();
+        KafkaConfig.BrokerHosts hosts = TridentKafkaConfig.StaticHosts.fromHostString(ImmutableList.of(testKafkaBrokerHost), 1);
+        TridentKafkaConfig config = new TridentKafkaConfig(hosts, "test");
+        config.scheme = new SchemeAsMultiScheme(new StringScheme());
+        return new TransactionalTridentKafkaSpout(config);
+    }
+
 }
