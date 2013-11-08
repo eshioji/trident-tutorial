6875cee0e9f1446e484af3ffe9577477119d0ff7
Enno Shioji <eshioji@gmail.com>
2013-11-08 10:39:37 +0000

    Reorganize packages

--- src/main/java/tutorial/storm/trident/operations/OnlyHashtags.java
+++ src/main/java/tutorial/storm/trident/operations/OnlyHashtags.java
@@ -1,20 +1,20 @@
-package tutorial.storm.trident.example;
+package tutorial.storm.trident.operations;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import storm.trident.operation.BaseFilter;
 import storm.trident.operation.Filter;
 import storm.trident.tuple.TridentTuple;
-import twitter4j.User;
+import tutorial.storm.trident.testutil.Content;
 
 
 /**
  * @author Enno Shioji (enno.shioji@peerindex.com)
  */
-public class OnlyEnglish extends BaseFilter {
+public class OnlyHashtags extends BaseFilter {
     @Override
     public boolean isKeep(TridentTuple tuple) {
-        User user = (User)tuple.get(0);
-        return "en".equals(user.getLang());
+        Content content = (Content)tuple.get(0);
+        return "hashtag".equals(content.getContentType());
     }
 }
