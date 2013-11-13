8966ec7b22fa8c9bdea08859350d532ece4394e4
Enno Shioji <eshioji@gmail.com>
2013-11-13 13:26:17 +0000

    Remove wrong import

--- src/main/java/tutorial/storm/trident/Skeleton.java
+++ src/main/java/tutorial/storm/trident/Skeleton.java
@@ -26,7 +26,6 @@ import tutorial.storm.trident.testutil.TweetIngestor;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.UUID;
 
 import static com.google.common.base.Preconditions.checkArgument;
