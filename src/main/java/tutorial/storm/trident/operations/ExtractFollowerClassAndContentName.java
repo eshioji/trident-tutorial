d46b20c4a143c5a2e3a439b9039b9a2f087f1ee9
Enno Shioji <eshioji@gmail.com>
2014-07-28 15:38:36 +0100

    Remove RangeMap because we have an incompatible version somewhere that breaks with "IncompatibleClass". Can't find the culprit though.

--- src/main/java/tutorial/storm/trident/operations/ExtractFollowerClassAndContentName.java
+++ src/main/java/tutorial/storm/trident/operations/ExtractFollowerClassAndContentName.java
@@ -1,9 +1,6 @@
 package tutorial.storm.trident.operations;
 
 import backtype.storm.tuple.Values;
-import com.google.common.collect.ImmutableRangeMap;
-import com.google.common.collect.Range;
-import com.google.common.collect.RangeMap;
 import storm.trident.operation.BaseFunction;
 import storm.trident.operation.TridentCollector;
 import storm.trident.tuple.TridentTuple;
@@ -15,28 +12,26 @@ import twitter4j.User;
  * @author Enno Shioji (enno.shioji@peerindex.com)
  */
 public class ExtractFollowerClassAndContentName extends BaseFunction {
-    private RangeMap<Integer, String> discretizationMap;
     @Override
     public void execute(TridentTuple tuple, TridentCollector collector) {
-        if(discretizationMap == null) discretizationMap = getDiscretizationMap();
-
         Content content = (Content)tuple.get(0);
         User user = (User)tuple.get(1);
 
-        String followerClass = discretizationMap.get(user.getFollowersCount());
+        String followerClass = classify(user.getFollowersCount());
 
         collector.emit(new Values(followerClass, content.getContentName()));
     }
 
-    private RangeMap<Integer, String> getDiscretizationMap() {
-        ImmutableRangeMap.Builder<Integer,String> b = ImmutableRangeMap.builder();
-        b.put(  Range.lessThan(10),                                     "< 10");
-        b.put(Range.closedOpen(10,                 100),                "10-100");
-        b.put(Range.closedOpen(100,               1000),                "100-1K");
-        b.put(Range.closedOpen(1000,         10 * 1000),                "1K-10K");
-        b.put(Range.closedOpen(10 * 1000,   100 * 1000),                "10K-100K");
-        b.put(Range.closedOpen(100 * 1000, 1000 * 1000),                "100K-1M");
-        b.put(Range.greaterThan(1000 * 1000),                           "> 1M");
-        return b.build();
+    private String classify(int followersCount) {
+        if (followersCount < 100){
+            return "< 100";
+        } else if (followersCount < 10*1000){
+            return "< 10K";
+        } else if (followersCount < 100*1000){
+            return "< 100K";
+        } else {
+            return ">= 100K";
+        }
     }
+
 }
